1b29465a2405fafe86aa8ad1434c1a959c4eae77
Farbod909 <rafezyfarbod@gmail.com>
2016-01-08 21:24:36 -0800

    Completes previous commit

---
+++ MorScout/HomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  HomeVC.swift
+//  MorScout
+//
+//  Created by Farbod Rafezy on 1/8/16.
+//  Copyright © 2016 MorTorq. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
