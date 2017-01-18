bc234526dd357f0e82201cf8be0cd8089e3b284d
Farbod909 <rafezyfarbod@gmail.com>
2017-01-18 13:18:51 -0800

    Update String subscript extension for swift 3

--- MorScout/Supporting Files/Util.swift
+++ MorScout/Supporting Files/Util.swift
@@ -273,27 +273,30 @@ func getCurrentRegionalKey() {
 
 extension String {
     
-    subscript (i: Int) -> Character {
-        return self[self.characters.index(self.startIndex, offsetBy: i)]
+    var length: Int {
+        return self.characters.count
     }
     
     subscript (i: Int) -> String {
-        return String(self[i] as Character)
+        return self[Range(i ..< i + 1)]
+    }
+    
+    func substring(from: Int) -> String {
+        return self[Range(min(from, length) ..< length)]
+    }
+    
+    func substring(to: Int) -> String {
+        return self[Range(0 ..< max(0, to))]
     }
     
     subscript (r: Range<Int>) -> String {
-        return substring(with: (characters.index(startIndex, offsetBy: r.lowerBound) ..< characters.index(startIndex, offsetBy: r.upperBound)))
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return self[Range(start ..< end)]
     }
     
-    var first: String {
-        return String(characters.prefix(1))
-    }
-    var last: String {
-        return String(characters.suffix(1))
-    }
-    var capitalized: String {
-        return first.uppercased() + String(characters.dropFirst())
-    }
 }
 
 extension UIViewController {
